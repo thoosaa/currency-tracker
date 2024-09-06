3a4924f375f6a05a2a2cec4e736256b2d46736b1
Tatyana Sugako <tatyanasugako@gmail.com>
2024-09-06 11:16:23 +0300

    add: globals.scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import "styles/globals.scss";
+
 export const App: React.FC = () => {
   return <h1>Hello</h1>;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       constants: path.resolve(__dirname, "src/constants/"),
       pages: path.resolve(__dirname, "src/pages/"),
       store: path.resolve(__dirname, "src/store/"),
+      styles: path.resolve(__dirname, "src/styles/"),
       utils: path.resolve(__dirname, "src/utils/"),
     },
   },
