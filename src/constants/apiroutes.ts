2bf24af33d7900d21642e3a330fdaa72f74e85be
Tatyana Sugako <tatyanasugako@gmail.com>
2024-09-19 10:34:11 +0300

    feat: quotes block

--- src/constants/apiroutes.ts
+++ src/constants/apiroutes.ts
@@ -1,3 +1,2 @@
 export const API_KEY_QUOTE = "9D8D508E-EF66-459C-949A-822557F5DAF0";
-export const API_KEY_STOCK = " B9836106-9644-4746-BF95-1F263FB377E6";
 export const BASE_URL = "https://rest.coinapi.io/v1";
